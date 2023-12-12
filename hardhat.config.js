f9378626381cea6dd5dd80e6b08c9fbfa41e9868
honestfreak <me21b006@iittp.ac.in>
2023-12-12 15:56:25 +0530

    deployed to xrp evm sidechain

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,18 @@ require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    xrplEvmSidechain: {
+      url: "https://rpc-evm-sidechain.xrpl.org",
+      chainId: 1440002,
+      accounts: {
+        mnemonic: ""
+      }
+    },
+  },
   paths: {
     artifacts: "./src/backend/artifacts",
     sources: "./src/backend/contracts",
@@ -9,3 +21,7 @@ module.exports = {
     tests: "./src/backend/test"
   },
 };
+
+
+
+
